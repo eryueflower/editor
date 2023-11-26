cde1669fa2b2156816a1fc71a2ee7382cf28038b
李杰锋 <2593932181@qq.com>
2023-11-26 16:55:00 +0800

    22

--- packages/sfc-playground/src/Header.vue
+++ packages/sfc-playground/src/Header.vue
@@ -54,7 +54,7 @@ function toggleDark() {
   <nav>
     <h1>
       <img alt="logo" src="/logo.svg" />
-      <span>Vue SFC Playground</span>
+      <span>Wint SFC Playground</span>
     </h1>
     <div class="links">
       <VersionSelect
